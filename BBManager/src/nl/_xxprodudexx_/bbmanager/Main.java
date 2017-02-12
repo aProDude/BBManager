73e4d5e000f12434b07c03227de6fcc349cc5652
aProDude <info.produde@gmail.com>
2017-02-12 19:32:37 +0100

    Initial Commit

---
+++ BBManager/src/nl/_xxprodudexx_/bbmanager/Main.java
@@ -0,0 +1,24 @@
+package nl._xxprodudexx_.bbmanager;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+	
+	private static Main instance;
+	
+	@Override
+	public void onEnable(){
+		instance = this;
+	}
+	
+	@Override
+	public void onDisable(){
+		instance = null;
+	}
+	
+	public static Main getInstance(){
+		return instance;
+	}
+	
+
+}
