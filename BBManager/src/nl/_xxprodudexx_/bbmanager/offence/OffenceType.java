2e167edbf2f0ced68b93127fa57254201fecacf2
aProDude <info.produde@gmail.com>
2017-02-12 22:30:35 +0100

    Added new source

--- BBManager/src/nl/_xxprodudexx_/bbmanager/offence/OffenceType.java
+++ BBManager/src/nl/_xxprodudexx_/bbmanager/offence/OffenceType.java
@@ -3,7 +3,13 @@ package nl._xxprodudexx_.bbmanager.offence;
 public enum OffenceType {
 
 	KILLAURA(Category.HACK, "Kill-Aura", 50),
+	KILLAURA_HS(Category.HACK, "Kill-Aura (HeadSpin)", 50),
+	REACH(Category.HACK, "Reach", 50),
+	ANTIKNOCKBACK(Category.HACK, "AntiKnockback", 50),
+	
 	SPAM(Category.CHAT, "Spam", 15),
+	
+	
 	BAN_EVADE(Category.ACCOUNT, "Ban-Evade", 100); 
 
 	private String name;
