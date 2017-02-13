bf1007f09d5a760eaafe7bd0a030627a6ff39f5a
aProDude <info.produde@gmail.com>
2017-02-13 22:01:13 +0100

    Initial Commit

---
+++ BBManager/src/nl/_xxprodudexx_/bbmanager/listener/BadBehaviourListener.java
@@ -0,0 +1,15 @@
+package nl._xxprodudexx_.bbmanager.listener;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+
+import nl._xxprodudexx_.bbmanager.event.BadBehaviourEvent;
+
+public class BadBehaviourListener implements Listener {
+
+	@EventHandler
+	public void on(BadBehaviourEvent e) {
+
+	}
+
+}
