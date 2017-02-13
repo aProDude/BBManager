e4d19127226a4f769a29b4ca8507c2d9c820759d
aProDude <info.produde@gmail.com>
2017-02-13 16:45:14 +0100

    Fixed API Bug

--- BBManager/src/nl/_xxprodudexx_/bbmanager/api/BBManagerAPI.java
+++ BBManager/src/nl/_xxprodudexx_/bbmanager/api/BBManagerAPI.java
@@ -10,49 +10,76 @@ import nl._xxprodudexx_.bbmanager.util.BBPlayer;
 public class BBManagerAPI {
 
 	public void addBBPoints(UUID uuid, int points) {
-		BBPlayer p = new BBPlayer(uuid);
-		p.addBBPoints(points);
-		p.updateProfile();
-	} 
+		if (this.getBBPlayer(uuid) != null) {
+			BBPlayer p = this.getBBPlayer(uuid);
+			p.addBBPoints(points);
+			p.updateProfile();
+		}
+	}
 
 	public void addBBWarning(UUID uuid) {
-		BBPlayer p = new BBPlayer(uuid);
-		p.addBBWarning();
-		p.updateProfile();
+		if (this.getBBPlayer(uuid) != null) {
+			BBPlayer p = this.getBBPlayer(uuid);
+			p.addBBWarning();
+			p.updateProfile();
+		}
 	}
 
 	public void setBBPoints(UUID uuid, int points) {
-		BBPlayer p = new BBPlayer(uuid);
-		p.setBBPoints(points);
-		p.updateProfile();
+		if (this.getBBPlayer(uuid) != null) {
+			BBPlayer p = this.getBBPlayer(uuid);
+			p.setBBPoints(points);
+			p.updateProfile();
+		}
 	}
 
 	public void setBBWarnings(UUID uuid, int warnings) {
-		BBPlayer p = new BBPlayer(uuid);
-		p.setBBWarnings(warnings);
-		p.updateProfile();
+		if (this.getBBPlayer(uuid) != null) {
+			BBPlayer p = this.getBBPlayer(uuid);
+			p.setBBWarnings(warnings);
+			p.updateProfile();
+		}
 	}
 
 	public void setBanned(UUID uuid, boolean banned) {
-		BBPlayer p = new BBPlayer(uuid);
-		p.setBanned(banned);
-		p.updateProfile();
+		if (this.getBBPlayer(uuid) != null) {
+			BBPlayer p = this.getBBPlayer(uuid);
+			p.setBanned(banned);
+			p.updateProfile();
+		}
 	}
 
 	public int getBBPoints(UUID uuid) {
-		return new BBPlayer(uuid).getBBPoints();
+		if (this.getBBPlayer(uuid) != null) {
+			return this.getBBPlayer(uuid).getBBPoints();
+		} else {
+			return 0;
+		}
 	}
 
 	public int getBBWarnings(UUID uuid) {
-		return new BBPlayer(uuid).getBBWarnings();
+		if (this.getBBPlayer(uuid) != null) {
+			return this.getBBPlayer(uuid).getBBWarnings();
+		} else {
+			return 0;
+		}
 	}
 
 	public boolean isBanned(UUID uuid) {
-		return new BBPlayer(uuid).isBanned();
+		if (this.getBBPlayer(uuid) != null) {
+			return this.getBBPlayer(uuid).isBanned();
+		} else {
+			return false;
+		}
 	}
 
 	public BBPlayer getBBPlayer(UUID uuid) {
-		return new BBPlayer(uuid);
+		for (BBPlayer bbp : Main.getInstance().getBBPlayers()) {
+			if (bbp.getUUID().equals(uuid)) {
+				return bbp;
+			}
+		}
+		return null;
 	}
 
 	public BBPlayer getBBPlayer(Player p) {
