0f573a91e89a5cc7c8820496516a9c762123afc5
aProDude <info.produde@gmail.com>
2017-02-14 20:26:41 +0100

    Re-added String-constructor

--- BBManager/src/nl/_xxprodudexx_/bbmanager/util/YamlFile.java
+++ BBManager/src/nl/_xxprodudexx_/bbmanager/util/YamlFile.java
@@ -14,12 +14,6 @@ public class YamlFile {
 	private File file;
 	private FileConfiguration config;
 
-	// private static YamlFile playerDataFile = new YamlFile("playerdata");
-
-	// public static YamlFile getPlayerDataFile() {
-	// return playerDataFile;
-	// }
-
 	/**
 	 * This method generates a new yml-file. The name can be set using the
 	 * constructor-paramter 'String fileName'
@@ -27,27 +21,27 @@ public class YamlFile {
 	 * @param fileName
 	 *            ^^
 	 */
-	// public YamlFile(String fileName) {
-	// this.name = fileName;
-	// if (!Main.getInstance().getDataFolder().exists()) {
-	// Main.getInstance().getDataFolder().mkdirs();
-	// }
-	//
-	// File dataFolder = Main.getInstance().getDataFolder();
-	// file = new File(dataFolder, fileName + ".yml");
-	//
-	// if (!file.exists()) {
-	// try {
-	// file.createNewFile();
-	// } catch (Exception e) {
-	// e.printStackTrace();
-	// }
-	// }
-	//
-	// config = YamlConfiguration.loadConfiguration(file);
-	// config.options().header("This is " + fileName + ".yml - file.");
-	// this.save();
-	// }
+	public YamlFile(String fileName) {
+		this.name = fileName;
+		if (!Main.getInstance().getDataFolder().exists()) {
+			Main.getInstance().getDataFolder().mkdirs();
+		}
+
+		File dataFolder = Main.getInstance().getDataFolder();
+		file = new File(dataFolder, fileName + ".yml");
+
+		if (!file.exists()) {
+			try {
+				file.createNewFile();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+
+		config = YamlConfiguration.loadConfiguration(file);
+		config.options().header("This is " + fileName + ".yml - file.");
+		this.save();
+	}
 
 	public YamlFile(UUID uuid) {
 		this.name = uuid.toString();
